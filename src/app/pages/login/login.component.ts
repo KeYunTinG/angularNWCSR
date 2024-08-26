8454ee445abec0a5af5cd6a581184d1a5db96c5a
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-08-26 09:11:59 +0800

    login to zh-TW

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -6,9 +6,9 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   isPasswordVisible: boolean = false;
@@ -16,21 +16,19 @@ export class LoginComponent {
   password: string = '';
 
   constructor(private router: Router) {}
- //密碼隱藏/顯示
+  //密碼隱藏/顯示
   togglePasswordVisibility(): void {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
   //登入邏輯
   signin(): void {
-    console.log("username:",this.username);
-    console.log("password:",this.password);
+    console.log('username:', this.username);
+    console.log('password:', this.password);
 
-    if(this.username.length === 0 || this.password.length === 0)
-      return;
+    if (this.username.length === 0 || this.password.length === 0) return;
     if (this.username !== 'mygo' || this.password !== 'mygo') {
-      alert('帳號密碼有誤')
-    }
-    else if (this.username === 'mygo' && this.password === 'mygo') {
+      alert('員工編號密碼有誤');
+    } else if (this.username === 'mygo' && this.password === 'mygo') {
       this.router.navigate(['/default']);
     }
   }
